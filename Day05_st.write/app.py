13ad2ab46e6bb653fea348eb88293424b67bfe8f
omkargend455 <omkargend4557@gmail.com>
2026-03-03 17:40:51 +0530

    Day 05: Use of st.write

---
+++ Day05_st.write/app.py
@@ -0,0 +1,32 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('st.write')
+
+# Example 1
+st.write("Hello *World!* :sunglasses:")
+
+# Example 2
+st.write(1234)
+
+# example 3
+df = pd.DataFrame({
+    "first column" : [1,2,3,4],
+    "second column" : [10,20,30,40]
+})
+
+# example 4
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+st.divider()
+
+# example 5
+st.write("Plotting Scatter plot of 200rows*3columns random points")
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
