2dd3fe833bdda367b4bc1b54a94d8d33e7720550
omkargend455 <omkargend4557@gmail.com>
2026-03-06 16:13:34 +0530

    Day 08: Exploring use of st.slider

---
+++ Day08_st.slider/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from datetime import time, datetime
+
+st.header("st.slider")
+
+# Ex 1
+
+st.subheader("Slider")
+
+age = st.slider("How old are you?", 0, 130, 25)
+st.write("I'm", age , "years old")
+
+# Ex 2
+st.subheader("Range slider")
+
+values = st.slider(
+    "Select a range of values",
+    0.0, 100.0, (25.0, 75.0))
+st.write("Values:", values)
