6283f77efca950de17f49a3da585fdf9b6e0856b
omkargend455 <omkargend4557@gmail.com>
2026-03-08 16:03:28 +0530

    Day 08: Exploring use of st.slider

--- Day08_st.slider/app.py
+++ Day08_st.slider/app.py
@@ -17,3 +17,22 @@ values = st.slider(
     "Select a range of values",
     0.0, 100.0, (25.0, 75.0))
 st.write("Values:", values)
+
+
+# Ex.3 
+st.subheader("Range time slider")
+appointment = st.slider(
+    "Schedule your appointment:", 
+    value=(time(11, 30), time(12, 45)))
+
+st.write("You are scheduled for:", appointment)
+
+# Ex 4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
